8bbb713e170be41d1cb7ec3508309c716abf880a
isaacfengamp <isaacfengampaire@gmail.com>
2024-01-23 17:56:48 +0300

    isaac

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,28 +33,46 @@ export default function Navbar() {
       },
     ]
   return (
-    <header>
-      <h1>Ampaire</h1>
-      <nav className= {showNavi? "shownave": ""}>
-         {
-            navItems.map((item,i) =>{return(
-                <Link key={i} href={item.path}>{item.title}</Link>
-            )})}
-      </nav>
-      <div className="socailIcons">
-        <FaFacebook/>
-        <FaTwitter/>
-        <FaLinkedin/>
-        <FaInstagram/>
-        <FaSpotify/>
+    <div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <div className="humberg">
-        <button onClick={showNavfunc}>
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-      </div>
-    </header>
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li>
+          <a>About</a>
+          <ul class="p-2">
+            <li><a>Bio Data</a></li>
+            <li><a>Experience</a></li>
+          </ul>
+        </li>
+        <li><a>Services</a></li>
+        
+      </ul>
+    </div>
+    <a class="btn btn-ghost text-xl">Ampaire!</a>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li>
+        <details>
+          <summary>About</summary>
+          <ul class="p-2">
+            <li><a>Bio Data</a></li>
+            <li><a>Experience</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Services</a></li>
+     
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <a class="btn">Contact</a>
+  </div>
+</div>
   )
 }
