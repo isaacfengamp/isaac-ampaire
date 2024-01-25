cb4dfdf363c9aff16b7f9ed453fa95c58b8308ae
isaacfengamp <isaacfengampaire@gmail.com>
2024-01-25 17:20:39 +0300

    feng

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,7 @@
 export default function Hero() {
   return (
     <main>
+      
         <h2>Hello!, I Am</h2>
         <h1 className="heroheader"> AMPAIRE ISAAC</h1>
         <h2>Explore more about Me</h2>
